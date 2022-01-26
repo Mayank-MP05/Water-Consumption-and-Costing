3a17d3c4185b0f76ade1d24c0a37b9162e3ed9d7
Mayank-MP05 <mayank5pande@gmail.com>
2022-01-26 16:42:58 +0530

    Test and Requirement.txt Added for Dependancy Management.

---
+++ test_calculate.py
@@ -0,0 +1,26 @@
+import unittest
+import math
+
+from fn_add_guest import calculate_for_add_guests
+from fn_allot_water import calculate_for_allot_water
+
+class UnitTestCase(unittest.TestCase):
+
+    def test_ALLOT_WATER(self):
+        consumption,cost = calculate_for_allot_water(2,0.5)
+        self.assertEqual(consumption,900)
+        self.assertEqual(int(cost),1200)
+        
+        consumption,cost = calculate_for_allot_water(3,2.0)
+        self.assertEqual(consumption,1500)
+        self.assertEqual(cost,1750)
+        
+    def test_ADD_GUEST(self):
+        consumption,cost = calculate_for_add_guests(5)
+        self.assertEqual(consumption,1500)
+        self.assertEqual(cost,4000)
+        
+        consumption,cost = calculate_for_add_guests(1)
+        self.assertEqual(consumption,300)
+        self.assertEqual(cost,600)
+     
--- tests/test_file_opener.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-class UnitTestCase(unittest.TestCase):
-    
-    def test_file_opener(self):
-        self.assertEqual('foo'.upper(), 'FOO')
