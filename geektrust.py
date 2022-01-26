3cd48e2dc0ce8c68a071e123c0043ae2caf8218a
Mayank-MP05 <mayank5pande@gmail.com>
2022-01-26 14:38:25 +0530

    Logic Implementation for command ALLOT_WATER

--- geektrust.py
+++ geektrust.py
@@ -1,13 +1,26 @@
 import sys
 
+from constant import ALLOT_WATER
+
+net_water_consumed = 0
+cost_of_water_consumed = 0
+
 def main():
     input_file = sys.argv[1]
     opened_file_ptr = open(input_file,'r')
+
     while True:
-        line = opened_file_ptr.readline()
-        if not line:
+        command = opened_file_ptr.readline()
+        if not command:
             break
-        print("---",line)
+        parsed_commands_with_args = command.split(" ")
+        command_alias = parsed_commands_with_args[0]
+        
+        if( command_alias == ALLOT_WATER ):
+            apartment_type = int(parsed_commands_with_args[1])
+            ratio_extraction_string = parsed_commands_with_args[2].split(":")
+            corporation_to_bore_ration = int(ratio_extraction_string[0])/int(ratio_extraction_string[1])
+            print(corporation_to_bore_ration)
 
         
 
